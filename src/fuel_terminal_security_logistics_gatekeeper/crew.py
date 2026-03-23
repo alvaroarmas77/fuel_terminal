23fcb5eaeef06a2d7ecccf086f79995a2380c0a8
unknown <alvaroarmas@gmail.com>
2026-03-23 16:37:20 -0500

    1637

--- src/fuel_terminal_security_logistics_gatekeeper/crew.py
+++ src/fuel_terminal_security_logistics_gatekeeper/crew.py
@@ -21,7 +21,7 @@ class FuelTerminalSecurityLogisticsGatekeeperCrew():
             api_key=os.getenv("GOOGLE_API_KEY"),
             temperature=0.1,
             max_rpm=2,
-            timeout=120,    # Damos más tiempo (2 min) para que el servidor responda
+            timeout=300,    # Damos más tiempo (2 min) para que el servidor responda
             max_retries=3   # Si se desconecta, que lo intente 3 veces automáticamente
         )
 
--- src/fuel_terminal_security_logistics_gatekeeper/main.py
+++ src/fuel_terminal_security_logistics_gatekeeper/main.py
@@ -26,8 +26,8 @@ def run():
     inputs = {
         'dispatcher_email': 'cliente_prueba@empresa.com',
         'driver_email': 'conductor1@transporte.com, conductor2@transporte.com', 
-        'driver_name': 'Juan Pérez, Ricardo Gómez',
-        'truck_plate': 'ABC-1234, XYZ-9876', 
+        'driver_name': 'Juan Pérez',#, Ricardo Gómez',#
+        'truck_plate': 'ABC-1234',#, XYZ-9876', #
         'requested_datetime': ahora.strftime('%Y-%m-%dT%H:00:00'), 
         'current_date': ahora.strftime('%Y-%m-%d'),
         'location': 'Terminal Sur - Surquillo',
